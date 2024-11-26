1b8fe3fca5a1f43bab17cb7c73d1d22dbfb1ea65
Ashik80 <ashikurrahman020995@gmail.com>
2024-11-26 19:22:32 +0600

    refactor: clean up token generation functions

--- accessor/jwt_token.go
+++ accessor/jwt_token.go
@@ -28,11 +28,47 @@ type JWTAccess interface {
 	GetSigningMethod() jwt.SigningMethod
 }
 
-func NewToken(ctx context.Context, a JWTAccess, claims *claims.JWTClaims, opt *options.AuthOptions) (*Token, error) {
+func NewToken(ctx context.Context, a JWTAccess, c *claims.JWTClaims, opt *options.AuthOptions) (*Token, error) {
+	key, err := GetParsedSigningKey(a)
+	if err != nil {
+		return nil, err
+	}
+
+	accessToken, err := GenerateAccessToken(a, c.AccessClaims, key)
+	if err != nil {
+		return nil, err
+	}
+
+	tok := &Token{
+		AccessToken: accessToken,
+		TokenType:   "Bearer",
+		ExpiresIn:   opt.Validity.AccessExpiresIn,
+	}
+
+	if c.IdClaims != nil {
+		idToken, err := GenerateIdToken(a, c.IdClaims, key)
+		if err != nil {
+			return nil, err
+		}
+		tok.IdToken = idToken
+	}
+
+	refreshExpiresIn := opt.Validity.RefreshExpiresIn
+	if refreshExpiresIn != 0 {
+		refresh, err := GenerateRefreshToken(ctx, accessToken, opt)
+		if err != nil {
+			return nil, err
+		}
+		tok.RefreshToken = refresh
+	}
+
+	return tok, nil
+}
+
+func GetParsedSigningKey(a JWTAccess) (interface{}, error) {
 	signingMethod := a.GetSigningMethod()
 	signedKey := a.GetSignedKey()
 
-	// get appropriate signing key
 	var key interface{}
 	if strings.HasPrefix(signingMethod.Alg(), "RS") {
 		privateKey, err := jwt.ParseRSAPrivateKeyFromPEM(signedKey)
@@ -44,47 +80,42 @@ func NewToken(ctx context.Context, a JWTAccess, claims *claims.JWTClaims, opt *o
 		key = signedKey
 	}
 
-	// generate access token
-	token := jwt.NewWithClaims(signingMethod, claims.AccessClaims)
+	return key, nil
+}
+
+func GenerateAccessToken(a JWTAccess, accessClaims *claims.JWTAccessClaims, signingKey interface{}) (string, error) {
+	token := jwt.NewWithClaims(a.GetSigningMethod(), accessClaims)
 	token.Header["kid"] = a.GetSignedKeyID()
-	access, err := token.SignedString(key)
+	accessToken, err := token.SignedString(signingKey)
 	if err != nil {
-		return nil, fmt.Errorf("failed to sign token: %w", err)
+		return "", fmt.Errorf("failed to sign token: %w", err)
+	}
+	return accessToken, nil
+}
+
+func GenerateIdToken(a JWTAccess, idClaims *claims.JWTIdClaims, signingKey interface{}) (string, error) {
+	idToken := jwt.NewWithClaims(a.GetSigningMethod(), idClaims)
+	idToken.Header["kid"] = a.GetSignedKeyID()
+	idString, err := idToken.SignedString(signingKey)
+	if err != nil {
+		return "", fmt.Errorf("failed to sign token: %w", err)
+	}
+	return idString, nil
+}
+
+func GenerateRefreshToken(ctx context.Context, accessToken string, opt *options.AuthOptions) (string, error) {
+	id := uuid.NewSHA1(uuid.New(), []byte(accessToken)).String()
+	refresh := base64.URLEncoding.EncodeToString([]byte(id))
+	expiry := opt.Validity.RefreshExpiresIn
+
+	ti := &store.TokenInfo{
+		ResourceOwnerId: id,
+		AccessToken:     accessToken,
+		Expiry:          time.Now().Add(time.Duration(expiry) * time.Second),
+	}
+	if err := opt.Store.StoreToken(ctx, ti); err != nil {
+		return "", fmt.Errorf("failed to store token: %w", err)
 	}
 
-	tok := &Token{
-		AccessToken: access,
-		TokenType:   "Bearer",
-		ExpiresIn:   opt.Validity.AccessExpiresIn,
-	}
-
-	// generate id token
-	if claims.IdClaims != nil {
-		idToken := jwt.NewWithClaims(signingMethod, claims.IdClaims)
-		idToken.Header["kid"] = a.GetSignedKeyID()
-		idstring, err := idToken.SignedString(key)
-		if err != nil {
-			return nil, fmt.Errorf("failed to sign token: %w", err)
-		}
-		tok.IdToken = idstring
-	}
-
-	// generate refresh token
-	refreshExpiresIn := opt.Validity.RefreshExpiresIn
-	if refreshExpiresIn != 0 {
-		id := uuid.NewSHA1(uuid.New(), []byte(access)).String()
-		refresh := base64.URLEncoding.EncodeToString([]byte(id))
-
-		ti := &store.TokenInfo{
-			ResourceOwnerId: id,
-			AccessToken:     access,
-			Expiry:          time.Now().Add(time.Duration(refreshExpiresIn) * time.Second),
-		}
-		if err = opt.Store.StoreToken(ctx, ti); err != nil {
-			return nil, err
-		}
-		tok.RefreshToken = refresh
-	}
-
-	return tok, nil
+	return refresh, nil
 }
